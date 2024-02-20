499bffab428952bcd6905d54275abc9acf2249ee
Gabi <gabrysia.czajkowska@wp.pl>
2024-02-21 00:10:17 +0100

    entity and repository

--- src/main/java/com/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/sms/StudentManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.studentmanagementsystem;
+package com.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
