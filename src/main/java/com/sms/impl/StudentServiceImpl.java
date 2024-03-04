0133ee71a37f3f238b0a1c7708f0e2443f31af1a
Gabi <gabrysia.czajkowska@wp.pl>
2024-03-04 17:26:17 +0100

    Get Mapping method to handle delete request

--- src/main/java/com/sms/impl/StudentServiceImpl.java
+++ src/main/java/com/sms/impl/StudentServiceImpl.java
@@ -34,4 +34,10 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.save(student);
     }
 
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+
+    }
+
 }
